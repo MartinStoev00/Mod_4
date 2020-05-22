68c2dc4e9141801230698a963deba400eb2bf344
s2392593 <m.stoev@student.utwente.nl>
2020-05-22 10:36:03 +0200

    jabascript split

---
+++ Design/scripts/main.js
@@ -0,0 +1,29 @@
+export default function main(incoming) {
+    let headerBlock = document.getElementsByClassName("header")[0];
+    let headerH = headerBlock.getBoundingClientRect().height;
+    let mainBlock = document.getElementsByClassName("main")[0];
+	function fillMain() {
+        function ran(input) {
+            let num = Math.ceil(Math.random()*(input - 1));
+            return "folder/" + num + ".jpg";
+        }
+        if(document.title == "Main") {
+            incoming.items.forEach((item) => {
+            let {name, description, img} = item;
+            main.innerHTML += 
+                `<a href="${img}" class="main__card">
+                    <div class="main__container">
+                        <div style="background-image: url(${ran(23)});" class="main__img"></div>
+                        <div style="background-image: url(${img});" class="main__profile"></div>
+                        <div class="main__text">
+                            <p class="main__name">${name}</p>
+                        </div>
+                    </div>
+                </a>`
+            });
+        } 
+    }
+    mainBlock.style.marginTop = headerH + "px";
+    mainBlock.style.paddingBottom = headerH + "px";
+    fillMain();
+}
---
+++ Design/scripts/sidebar.js
@@ -0,0 +1,50 @@
+export default function sidebar(incoming) {
+	 function fillPeople() {
+        let people = document.getElementsByClassName("people")[0];
+        incoming.items.forEach((item) => {
+            let {name, img} = item;
+            people.innerHTML += 
+                `<a href="${img}" class="people__container">
+                    <div class="people__pic" style="background-image: url(${img});"></div>
+                    <div class="people__name">${name}</div>
+                </a>`
+        });
+    }
+    let currentpage;
+    sidebar.style.top = headerH + "px";
+    let sbc = document.getElementsByClassName("sidebar__content")[0];
+    let t = sbc.getBoundingClientRect().top + sbc.getBoundingClientRect().height;
+    document.getElementsByClassName("people")[0].style.top = "calc(5vh + " + t + "px)";
+    let linkForPages = document.getElementsByClassName("sidebar__link");
+    Array.prototype.forEach.call(linkForPages, (link, index) => {
+        let linkL = link.getAttribute("data-link");
+        function on() {
+            currentpage = linkL;
+            link.style.backgroundColor = "rgba(66, 133, 244, 0.1)";
+            link.style.color = "rgb(66, 133, 244)";
+        }
+        function off(linkAll) {
+            linkAll.style.backgroundColor = "#fff";
+            linkAll.style.color = "#444";
+        }
+        if(index == 0) {
+            on();
+        }
+        link.addEventListener("click", () => {
+            Array.prototype.forEach.call(linkForPages, (linkAll) => off(linkAll));
+            on();
+            console.log(linkL);
+        });
+        link.addEventListener("mouseover", () => {
+            if(currentpage !== linkL) {
+                link.style.backgroundColor = "#f5f5f5";
+            }
+        });
+        link.addEventListener("mouseout", () => {
+            if(currentpage !== linkL) {
+                off(link);
+            }
+        })
+    });
+    fillPeople();
+}
