4b5fc94bddb7bdd9c2b0fc793c172348631f4d97
Legitizer <Legitizer@gmail.com>
2020-05-27 13:18:42 +0200

    signup nearly done

--- caren/src/main/java/nl/nedap/core/Signup.java
+++ caren/src/main/java/nl/nedap/core/Signup.java
@@ -48,19 +48,21 @@ public class Signup extends HttpServlet {
 		String firstname = request.getParameter("firstname");
 		String lastname = request.getParameter("lastname");
 		String email = request.getParameter("email");
-		String birthday = request.getParameter("birthday");
+		String birthdate = request.getParameter("birthdate");
+		String gender = request.getParameter("gender");
 		String password = request.getParameter("password");
 		String passwordagn = request.getParameter("passwordagn");
 		
 		
 		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection conn = DriverManager.getConnection(URL, DBUSERNAME, DBPASS);
 			
 			//TODO check if account already exists
 			//The query
-			String q = "SELECT a.email"
-					+ "FROM accounts a"
-					+ "WHERE a.email = '?'";
+			String q = "SELECT a.email" + "\n"
+					+ "FROM accounts a" + "\n"
+					+ "WHERE a.email = ?;";
 			
 			//Create prepared statement object
 			PreparedStatement statement = conn.prepareStatement(q);
@@ -69,18 +71,47 @@ public class Signup extends HttpServlet {
 			statement.setString(1, email);
 			
 			//Result set of statement execution
-			ResultSet resultset = statement.executeQuery("");
+			ResultSet resultset = statement.executeQuery();
 			
-			//TODO What to do with result of check
-			System.out.println(resultset.getMetaData().getColumnCount());
+			if (email == "") {
+				out.println(docType + "<HTML> <body>No email was provided: "+email+"</body> </HTML>");
+				return;
+			}
+			
+			if (!resultset.next()) { // can create acc
+				out.println(docType + "<HTML> <body>Creating account..</body> </HTML>");
+				
+				//Making call to database to make account.
+				String createAccQ = "INSERT INTO accounts (`email`, `password`) VALUES (?, ?)"; // email, password.
+				PreparedStatement accCreationSt = conn.prepareStatement(createAccQ);
+				accCreationSt.setString(1, email); accCreationSt.setString(2, password);
+				accCreationSt.executeUpdate();
+				//Retrieving the new account's aid.
+				String aidQ = "SELECT a.aid FROM accounts a WHERE a.email = ?"; // email.
+				PreparedStatement aidSt = conn.prepareStatement(aidQ);
+				aidSt.setString(1, email);
+				ResultSet aidResultSet = aidSt.executeQuery();
+				aidResultSet.next();
+				int aid = aidResultSet.getInt(1);
+				
+				//Making call to database to make person in people.
+				String createPersQ = "INSERT INTO people (aid, first_name, last_name, date_of_birth, gender) VALUES (?, ?, ?, ?, ?, ?)";
+				PreparedStatement persCreationSt = conn.prepareStatement(createPersQ);
+				persCreationSt.setString(1, ""+aid); persCreationSt.setString(2, firstname); persCreationSt.setString(3, lastname); persCreationSt.setString(4, birthdate); persCreationSt.setString(5, gender);
+				persCreationSt.executeUpdate();
+				
+				out.println(docType + "<HTML> <body>Account created.</body> </HTML>");
+				
+			} else { // cannot create acc
+				out.println(docType + "<HTML> <body> You cannot create an acc with that email </body> </HTML>");
+			}
 			
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			out.println(docType + "<HTML> <body> Unexpected error has occured. </body> </HTML>");
 		}
-		
-		out.println(docType + "<HTML> <body> result html </body> </HTML>");
 	}
 
 	/**
