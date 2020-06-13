0693eb57d32c47f514c1a8aabe1395205dc5a6f7
omar elkady <omarmohamedanwarmohamedelkady@student.utwente.nl>
2020-06-14 00:21:43 +0200

    sanitisation for all input fields done.

--- caren/src/main/java/nl/nedap/utility/ForeignCharactersChecker.java
+++ caren/src/main/java/nl/nedap/utility/ForeignCharactersChecker.java
@@ -3,10 +3,21 @@ package nl.nedap.utility;
 public class ForeignCharactersChecker {
 
 	private static String passwordCharacters = "abdefghijklmnopqrstuvwxyz0123456789ABDEFGHIJKLMNOPQRSTUVWXYZ";
+	private static String emailCharacters = passwordCharacters+"@.-_";
 	
-	public static boolean hasForeignCharacters(String p) {
+	public static boolean basicHasForeignCharacters(String p) {//checker for all input fields except email
 		for (int i = 0; i < p.length(); i++) {
 			if (!passwordCharacters.contains(p.substring(i, i+1))) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public static boolean emailHasForeignCharacters(String p) {//checker for email input fields
+		for (int i = 0; i < p.length(); i++) {
+			if (!emailCharacters.contains(p.substring(i, i+1))) {
 				return true;
 			}
 		}
